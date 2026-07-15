2d94f6bd3f88f6d564d62b4657c165b38aae47ee
muhsintec <muhsintecn@gmail.com>
2026-07-15 13:17:12 +0300

    New week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Job 1
+        Job job1 = new Job();
+        job1._jobTitle = "Lead Technical Engineer & Owner";
+        job1._company = "MTES - Muhsin Technical Engineering Services Limited";
+        job1._startYear = 2020;
+        job1._endYear = 2026;
+
+        // Job 2
+        Job job2 = new Job();
+        job2._jobTitle = "Electronic Appliance Specialist";
+        job2._company = "Self Employment";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        
+        Resume myResume = new Resume();
+        myResume._name = "Namboobi Nagimesi Muhsin";
+
+        // Milestones
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Profile
+        
+        myResume.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Attributs
+
+    public string _name;
+    
+    
+    public List<Job> _jobs = new List<Job>();
+
+    // Resume print method
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // job display
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
